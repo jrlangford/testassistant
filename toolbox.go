8e056188455653197283088e3c6d98bf4b597480
Jonathan Robin Langford <jonathan.langford@schibsted.com.mx>
2015-11-15 16:13:02 -0600

    Add function that compares flaoting point values based on error percentage

--- toolbox.go
+++ toolbox.go
@@ -1,13 +1,21 @@
 package toolbox
 
 import (
-	"testing"
 	"math"
+	"testing"
 )
 
-func failOnFloatMismatch(expectedFloat, receivedFloat float64, t *testing.T) {
+func FailOnFloatMismatch(expectedFloat, receivedFloat float64, t *testing.T) {
 	epsilon := 1e-9
 	if math.Abs(expectedFloat-receivedFloat) >= epsilon {
 		t.Errorf("\nExpected float: %.20f\nReceived float: %.20f\nEpsilon:%g\n", expectedFloat, receivedFloat, epsilon)
+	}
+}
+
+func FailOnFloatToleranceExceedance(expectedFloat, tolerancePercentage, receivedFloat float64, t *testing.T) {
+	error := 1 - receivedFloat/expectedFloat
+	errorPercentage := 100 * error
+	if errorPercentage > tolerancePercentage {
+		t.Errorf("\nExpected float: %.20f\nReceived float: %.20f\nTolerance:%f%%\nError:%f%% (%f), \n", expectedFloat, receivedFloat, tolerancePercentage, errorPercentage, error)
 	}
 }
