2027c723505bcf8e134664d786f07d74a94e3b58
Jonathan Robin Langford <jonathan.langford@schibsted.com.mx>
2015-12-30 10:08:43 -0600

    Improve tests for floating point numbers. Add test for integers.

--- toolbox.go
+++ toolbox.go
@@ -3,19 +3,37 @@ package toolbox
 import (
 	"math"
 	"testing"
+	"fmt"
 )
+
+var Debug = false
 
 func FailOnFloatMismatch(expectedFloat, receivedFloat float64, t *testing.T) {
 	epsilon := 1e-9
-	if math.Abs(expectedFloat-receivedFloat) >= epsilon {
-		t.Errorf("\nExpected float: %.20f\nReceived float: %.20f\nEpsilon:%g\n", expectedFloat, receivedFloat, epsilon)
+	outputString := fmt.Sprintf("\nExpected float: %.20f\nReceived float: %.20f\nEpsilon:%g\n", expectedFloat, receivedFloat, epsilon)
+	if math.Abs(expectedFloat-receivedFloat) >= epsilon || math.IsNaN(receivedFloat) {
+		t.Error(outputString)
+	} else if Debug {
+		fmt.Print(outputString)
 	}
 }
 
 func FailOnFloatToleranceExceedance(expectedFloat, tolerancePercentage, receivedFloat float64, t *testing.T) {
 	error := 1 - receivedFloat/expectedFloat
 	errorPercentage := 100 * error
-	if errorPercentage > tolerancePercentage {
-		t.Errorf("\nExpected float: %.20f\nReceived float: %.20f\nTolerance:%f%%\nError:%f%% (%f), \n", expectedFloat, receivedFloat, tolerancePercentage, errorPercentage, error)
+	outputString := fmt.Sprintf("\nExpected float: %.20f\nReceived float: %.20f\nTolerance:%g%%\nError:%g%% (%f), \n", expectedFloat, receivedFloat, tolerancePercentage, errorPercentage, error)
+	if math.Abs(errorPercentage) > tolerancePercentage || math.IsNaN(receivedFloat) {
+		t.Error(outputString)
+	} else if Debug {
+		fmt.Print(outputString)
+	}
+}
+
+func FailOnIntMismatch(expectedInt, receivedInt int, t *testing.T) {
+	outputString := fmt.Sprintf("\nExpected int: %d\nReceived int: %d\n\n", expectedInt, receivedInt)
+	if receivedInt != expectedInt {
+		t.Error(outputString)
+	} else if Debug {
+		fmt.Print(outputString)
 	}
 }
