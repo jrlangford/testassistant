6860e15c432918e86a40308fcb58d74b95b8a34e
Jonathan Robin Langford <jonathan.langford@schibsted.com.mx>
2016-03-06 09:25:25 -0600

    Document package

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# Toolbox
-A go package with helper functions to run tests
+# toolbox
+--
+    import "github.com/jrlangford/toolbox"
+
+Package toolbox provides functions that simplify the test writing process and
+provide a standardized way to report failure.
+
+## Usage
+
+```go
+var Debug = false
+```
+Debug enables output printing when a test has not failed if it is set to 'true'
+
+#### func  FailOnFloatMismatch
+
+```go
+func FailOnFloatMismatch(expectedFloat, receivedFloat float64, t *testing.T)
+```
+FailOnFloatMismatch compares two floating point numbers and reports failure if
+thier difference exceeds 1e-9.
+
+#### func  FailOnFloatToleranceExceedance
+
+```go
+func FailOnFloatToleranceExceedance(expectedFloat, tolerancePercentage, receivedFloat float64, t *testing.T)
+```
+FailOnFloatToleranceExceedance compares two floating point numbers and reports
+failure if the error percentage exceeds tolerancePercentage.
+
+#### func  FailOnIntMismatch
+
+```go
+func FailOnIntMismatch(expectedInt, receivedInt int, t *testing.T)
+```
+FailOnIntMismatch compares two ints and reports failure if they are different.
+
+#### func  FailOnRegexMismatch
+
+```go
+func FailOnRegexMismatch(expectedPattern, receivedString string, t *testing.T)
+```
+FailOnRegexMismatch compares a string with a regex pattern and reports failure
+if they do not match.
+
+#### func  FailOnStringMismatch
+
+```go
+func FailOnStringMismatch(expectedString, receivedString string, t *testing.T)
+```
+FailOnStringMismatch compares two strings and reports failure if they are not
+equal.
--- toolbox.go
+++ toolbox.go
@@ -1,14 +1,17 @@
+//Package toolbox provides functions that simplify the test writing process and provide a standardized way to report failure.
 package toolbox
 
 import (
-	"math"
-	"testing"
 	"fmt"
+	"math"
 	"regexp"
+	"testing"
 )
 
+//Debug enables output printing when a test has not failed if it is set to 'true'
 var Debug = false
 
+//FailOnFloatMismatch compares two floating point numbers and reports failure if thier difference exceeds 1e-9.
 func FailOnFloatMismatch(expectedFloat, receivedFloat float64, t *testing.T) {
 	epsilon := 1e-9
 	outputString := fmt.Sprintf("\nExpected float: %.20f\nReceived float: %.20f\nEpsilon:%g\n", expectedFloat, receivedFloat, epsilon)
@@ -19,10 +22,11 @@ func FailOnFloatMismatch(expectedFloat, receivedFloat float64, t *testing.T) {
 	}
 }
 
+//FailOnFloatToleranceExceedance compares two floating point numbers and reports failure if the error percentage exceeds tolerancePercentage.
 func FailOnFloatToleranceExceedance(expectedFloat, tolerancePercentage, receivedFloat float64, t *testing.T) {
-	error := 1 - receivedFloat/expectedFloat
-	errorPercentage := 100 * error
-	outputString := fmt.Sprintf("\nExpected float: %.20f\nReceived float: %.20f\nTolerance:%g%%\nError:%g%% (%f), \n", expectedFloat, receivedFloat, tolerancePercentage, errorPercentage, error)
+	err := 1 - receivedFloat/expectedFloat
+	errorPercentage := 100 * err
+	outputString := fmt.Sprintf("\nExpected float: %.20f\nReceived float: %.20f\nTolerance:%g%%\nError:%g%% (%f), \n", expectedFloat, receivedFloat, tolerancePercentage, errorPercentage, err)
 	if math.Abs(errorPercentage) > tolerancePercentage || math.IsNaN(receivedFloat) {
 		t.Error(outputString)
 	} else if Debug {
@@ -30,6 +34,7 @@ func FailOnFloatToleranceExceedance(expectedFloat, tolerancePercentage, received
 	}
 }
 
+//FailOnIntMismatch compares two ints and reports failure if they are different.
 func FailOnIntMismatch(expectedInt, receivedInt int, t *testing.T) {
 	outputString := fmt.Sprintf("\nExpected int: %d\nReceived int: %d\n\n", expectedInt, receivedInt)
 	if receivedInt != expectedInt {
@@ -39,6 +44,7 @@ func FailOnIntMismatch(expectedInt, receivedInt int, t *testing.T) {
 	}
 }
 
+//FailOnStringMismatch compares two strings and reports failure if they are not equal.
 func FailOnStringMismatch(expectedString, receivedString string, t *testing.T) {
 	if receivedString != expectedString {
 		t.Log("String Mismatch")
@@ -53,6 +59,7 @@ func FailOnStringMismatch(expectedString, receivedString string, t *testing.T) {
 	}
 }
 
+//FailOnRegexMismatch compares a string with a regex pattern and reports failure if they do not match.
 func FailOnRegexMismatch(expectedPattern, receivedString string, t *testing.T) {
 	matched, _ := regexp.MatchString(expectedPattern, receivedString)
 
